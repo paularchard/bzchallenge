6fb5943b069e2cea746d9534dd3c91d1e077f9ca
Paul Archard <paul@localhost>
2018-08-22 16:29:46 -0400

    declaration and test cases for report generator

---
+++ include/report.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <map>
+
+// Build a report by merging the token/value pairs in vars into the template 
+// read from tpl. The tokens are encoded in the template using %token-name% and
+// must not contain whitespace. Literal % symbols must be escaped using %%.  
+// Unterminated tokens with cause a std::invalid_argument exception to be
+// thrown. Unmatched tokens will be replaced with an empty string.
+//
+std::string make_report(std::istream &tpl, const std::map<std::string, std::string> &vars)
+{
+    return std::string();
+}
---
+++ test/test_report.cpp
@@ -0,0 +1,153 @@
+#include <gtest/gtest.h>
+#include <report.hpp>
+#include <sstream>
+
+TEST(Report, TestEmptyData)
+{
+    try
+    {
+        std::stringstream ss;
+        std::string report = make_report(ss, std::map<std::string, std::string>());
+        ASSERT_EQ(report, std::string());
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestValidData)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "This is a %val% of make_report";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("val", "test"));
+
+        std::string report = make_report(ss, tokens);
+        ASSERT_EQ(report, std::string("This is a test of make_report"));
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestTokenWithNoSpace)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "This is a test of %val%_report";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("val", "make"));
+
+        std::string report = make_report(ss, tokens);
+        ASSERT_EQ(report, std::string("This is a test of make_report"));
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestEscapedDelimiter)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "I %val% this book at 15%% off";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("val", "purchased"));
+
+        std::string report = make_report(ss, tokens);
+        ASSERT_EQ(report, std::string("I purchased this book at 15% off"));
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestMultipleTokens)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "I %action% this %item% at Barnes & Noble";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("action", "purchased"));
+        tokens.insert(std::make_pair("item", "book"));
+
+        std::string report = make_report(ss, tokens);
+        ASSERT_EQ(report, std::string("I purchased this book at Barnes & Noble"));
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestMissingTokenValue)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "I %action% this %item% at Barnes & Noble";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("action", "purchased"));
+
+        std::string report = make_report(ss, tokens);
+        ASSERT_EQ(report, std::string("I purchased this  at Barnes & Noble"));
+    }
+    catch (std::exception &e)
+    {
+        FAIL() << "Unexpected exception thrown";
+    }
+}
+
+TEST(Report, TestUnterminatedToken)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "This is a %val of make_report";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("val", "test"));
+
+        std::string report = make_report(ss, tokens);
+        FAIL() << "Expected exception to be thrown";
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "caught exception " << e.what() << std::endl;
+        EXPECT_EQ(e.what(), std::string("invalid argument"));
+    }
+}
+
+TEST(Report, TestTokenWithSpace)
+{
+    try
+    {
+        std::stringstream ss;
+        ss << "This is a %token name% of make_report";
+
+        std::map<std::string, std::string> tokens;
+        tokens.insert(std::make_pair("token name", "test"));
+
+        std::string report = make_report(ss, tokens);
+        FAIL() << "Expected exception to be thrown";
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "caught exception " << e.what() << std::endl;
+        EXPECT_EQ(e.what(), std::string("invalid argument"));
+    }
+}
